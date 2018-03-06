d198e31f0b8749eb7cd7a4f0a7ecc5642ed9de26
GeoFro <georgeharoldfrost@gmail.com>
2018-03-06 21:44:36 +1100

    Added component methods

--- src/components/TennisDayCount.js
+++ src/components/TennisDayCount.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import '../stylesheets/TennisDayCount.css';
 
 class TennisDayCount extends Component {
+  calcPercentage(number) {
+    return((number * 100) + "%")
+  }
+  calcGoalProgress(total, goal) {
+    return this.calcPercentage(total/goal)
+  }
+  calcTotalDays(val, val2) {
+    return (val + val2)
+  }
   render() {
+
+    const TotalDays = this.calcTotalDays(this.props.competition, this.props.casual);
+
     return (
       <div className="TennisDayCount">
         <div className="total-days">
-          <span>Total Days: {this.props.total}</span>
+          <span>Total Days: {this.calcTotalDays(this.props.competition, this.props.casual)}</span>
         </div>
         <div className="competition-days">
           <span>Competition Days: {this.props.competition}</span>
@@ -15,7 +27,10 @@ class TennisDayCount extends Component {
           <span>Casual Days: {this.props.casual}</span>
         </div>
         <div className="goal-days">
-          <span>Goal: {this.props.goal}</span>
+          <span>Days Goal: {this.props.goal}</span>
+        </div>
+        <div className="goal-progress">
+          <span>Goal Progress: {this.calcGoalProgress(TotalDays, this.props.goal)}</span>
         </div>
       </div>
     )
